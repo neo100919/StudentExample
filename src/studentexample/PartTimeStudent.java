ebc932fea92f4f5511e18033550ef2fa33600ce5
neo100919 <amaramo@sheridancollege.ca>
2022-05-26 11:57:28 -0400

    Added part time student

---
+++ src/studentexample/PartTimeStudent.java
@@ -0,0 +1,26 @@
+
+package studentexample;
+
+/**
+ *
+ * @author mtama
+ */
+public class PartTimeStudent extends  Student
+{
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+}
